b51cff5a2d30fc6c03c0914279ccd59d2c9b8d05
Yamy <yamy19007@gmail.com>
2023-07-31 14:50:44 +0200

    done

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-	baseURL: 'https://api.themoviedb.org/3/discover/movie',
+	baseURL: 'http://api.themoviedb.org/3/discover/movie',
 	headers: {
 		accept: 'application/json',
 		Authorization:
